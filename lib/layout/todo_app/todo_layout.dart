91c7c839556745e47d708ce8e36b3b9b197e8a48
Salah <salahexe3@gmail.com>
2021-07-29 17:25:53 +0100

    add snackBars commit

--- lib/layout/todo_app/todo_layout.dart
+++ lib/layout/todo_app/todo_layout.dart
@@ -9,8 +9,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:intl/intl.dart';
 
-class TodoLayout extends StatelessWidget  {
-
+class TodoLayout extends StatelessWidget {
   //FocusNode focusNode = new FocusNode();
 
   var _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -19,125 +18,147 @@ class TodoLayout extends StatelessWidget  {
   var _timeController = TextEditingController();
   var _dateController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (BuildContext context) => AppCubit()..createDatabase(),
       child: BlocConsumer<AppCubit, AppStates>(
-        listener: (BuildContext context, AppStates state){
-          if(state is AppInsertDatabaseState){
+        listener: (BuildContext context, AppStates state) {
+          if (state is AppInsertDatabaseState) {
             Navigator.pop(context);
+            /*
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Inserted Successfully'),
+                backgroundColor: Colors.lightBlueAccent,
+                duration: const Duration(
+                  seconds: 1,
+                ),
+              ),
+            );
+             */
+          }
+          if (state is AppDeleteDatabaseState) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Deleted Successfully'),
+                backgroundColor: Colors.lightBlueAccent,
+                duration: const Duration(
+                  seconds: 1,
+                ),
+              ),
+            );
           }
         },
-        builder: (BuildContext context, AppStates state){
+        builder: (BuildContext context, AppStates state) {
           AppCubit cubit = AppCubit.get(context);
           return Scaffold(
             key: _scaffoldKey,
-            appBar: AppBar(title: Text(cubit.titles[cubit.currentIndex]), titleSpacing: 20.0,),
+            appBar: AppBar(
+              title: Text(cubit.titles[cubit.currentIndex]),
+              titleSpacing: 20.0,
+            ),
             body: ConditionalBuilder(
-              condition: state is! AppGetDatabaseLoadingState, //true, // tasks.length > 0,
+              condition: state is! AppGetDatabaseLoadingState,
+              //true, // tasks.length > 0,
               builder: (context) => cubit.screens[cubit.currentIndex],
-              fallback: (context) =>  Center(child: CircularProgressIndicator()),
+              fallback: (context) => Center(child: CircularProgressIndicator()),
             ),
             floatingActionButton: FloatingActionButton(
               onPressed: () {
-
-                if(cubit.isBottomSheetShown){
-
-                  if(_formKey.currentState.validate()){
-
+                if (cubit.isBottomSheetShown) {
+                  if (_formKey.currentState.validate()) {
                     cubit.insertToDatabase(
                       title: _titleController.text,
                       time: _timeController.text,
                       date: _dateController.text,
                     );
-
-
                   }
-
-                }
-                else{
+                } else {
                   cubit.changeBottomSheet(icon: Icons.add, isShow: true);
 
-                  _scaffoldKey.currentState.showBottomSheet(
+                  _scaffoldKey.currentState
+                      .showBottomSheet(
                         (context) => Container(
-                      padding: EdgeInsets.all(20.0),
-                      color: Colors.white,
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-
-                            defaultFormField(
-                              validFun: (String value){
-                                if(value.isEmpty)
-                                  return 'title must not be empty';
-                                return null;
-                              },
-                              text: "Task Title",
-                              keyBoardType: TextInputType.text,
-                              controller: _titleController,
-                              prefix: Icons.title,
+                          padding: EdgeInsets.all(20.0),
+                          color: Colors.white,
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                defaultFormField(
+                                  validFun: (String value) {
+                                    if (value.isEmpty)
+                                      return 'title must not be empty';
+                                    return null;
+                                  },
+                                  text: "Task Title",
+                                  keyBoardType: TextInputType.text,
+                                  controller: _titleController,
+                                  prefix: Icons.title,
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                defaultFormField(
+                                  onlyRead: true,
+                                  onTap: () {
+                                    showTimePicker(
+                                      context: context,
+                                      initialTime: TimeOfDay.now(),
+                                    ).then((value) {
+                                      _timeController.text =
+                                          value.format(context).toString();
+                                      //print(value.format(context));
+                                    });
+                                  },
+                                  validFun: (String value) {
+                                    if (value.isEmpty)
+                                      return 'time must not be empty';
+                                    return null;
+                                  },
+                                  text: "Task Time",
+                                  keyBoardType: TextInputType.datetime,
+                                  controller: _timeController,
+                                  prefix: Icons.watch_later_rounded,
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                defaultFormField(
+                                  onlyRead: true,
+                                  onTap: () {
+                                    showDatePicker(
+                                      context: context,
+                                      initialDate: DateTime.now(),
+                                      firstDate: DateTime.now(),
+                                      lastDate: DateTime.utc(
+                                          2025), //parse("2021-12-00"),
+                                    ).then((value) {
+                                      _dateController.text = DateFormat.yMMMd()
+                                          .format(value)
+                                          .toString();
+                                    });
+                                  },
+                                  validFun: (String value) {
+                                    if (value.isEmpty)
+                                      return 'date must not be empty';
+                                    return null;
+                                  },
+                                  text: "Task Date",
+                                  keyBoardType: TextInputType.datetime,
+                                  controller: _dateController,
+                                  prefix: Icons.calendar_today,
+                                ),
+                              ],
                             ),
-
-                            SizedBox(height: 15,),
-                            defaultFormField(
-                              onlyRead: true,
-                              onTap: (){
-                                showTimePicker(
-                                  context: context,
-                                  initialTime: TimeOfDay.now(),
-                                ).then((value) {
-                                  _timeController.text = value.format(context).toString();
-                                  //print(value.format(context));
-                                });
-                              },
-                              validFun: (String value){
-                                if(value.isEmpty)
-                                  return 'time must not be empty';
-                                return null;
-                              },
-                              text: "Task Time",
-                              keyBoardType: TextInputType.datetime,
-                              controller: _timeController,
-                              prefix: Icons.watch_later_rounded,
-                            ),
-
-
-                            SizedBox(height: 15,),
-                            defaultFormField(
-                              onlyRead: true,
-                              onTap: (){
-                                showDatePicker(
-                                  context: context,
-                                  initialDate: DateTime.now(),
-                                  firstDate: DateTime.now(),
-                                  lastDate: DateTime.utc(2025), //parse("2021-12-00"),
-                                ).then((value) {
-                                  _dateController.text = DateFormat.yMMMd().format(value).toString();
-                                });
-                              },
-                              validFun: (String value){
-                                if(value.isEmpty)
-                                  return 'date must not be empty';
-                                return null;
-                              },
-                              text: "Task Date",
-                              keyBoardType: TextInputType.datetime,
-                              controller: _dateController,
-                              prefix: Icons.calendar_today,
-                            ),
-
-
-
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                    elevation: 25.0,
-                  ).closed.then((value) {
+                        elevation: 25.0,
+                      )
+                      .closed
+                      .then((value) {
                     //closeBottomSheet();
                     cubit.changeBottomSheet(icon: Icons.edit, isShow: false);
 
@@ -146,7 +167,6 @@ class TodoLayout extends StatelessWidget  {
                     _timeController.clear();
                   });
                 }
-
 
                 // here we try to understand a little of Async Await Futures
                 /*
@@ -204,14 +224,13 @@ class TodoLayout extends StatelessWidget  {
     );
   }
 
-  /*
+/*
   // future async await : simple explanation:
   Future<String> getName() async{
     return "Salah Eddine";
   }*/
 
 }
-
 
 // 1. create database
 // 2. create tables
